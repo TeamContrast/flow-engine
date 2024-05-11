b7a7d28940b9dd71d59595b6f1e1284b182c7d19
coderman64 <coderman64@gmail.com>
2024-05-11 16:59:14 -0400

    new/improved rush-style boost bar!

--- flow_engine/scripts/UI/BoostBar.gd
+++ flow_engine/scripts/UI/BoostBar.gd
@@ -1,93 +1,57 @@
-extends VBoxContainer
-
-##Used to control the boost bar UI
+## Used to control the boost bar UI
+extends Control
 class_name FlowBoostBar
 
-##The texture for the boost bar units when they're empty
-const boost_bar_white:AtlasTexture = preload("res://flow_engine/sprites/UI/white_progress.tres")
-## The texture for the boost bar units when they're between empty and yellow
-const boost_bar_blue:AtlasTexture = preload("res://flow_engine/sprites/UI/blue_progress.tres")
-##The texture for the boost bar units when they're between red and blue
-const boost_bar_yellow:AtlasTexture = preload("res://flow_engine/sprites/UI/yellow_progress.tres")
-##The texture for the boost bar units when they're full
-const boost_bar_red:AtlasTexture = preload("res://flow_engine/sprites/UI/red_progress.tres")
+var barUnit = preload("res://flow_engine/UI/boost_bar_segment.tscn")
 
-##If true, Sonic the boost bar will always be full (infinite boost)
-@export var infiniteBoost:bool = false
+var barItems = []
 
-#var growMode = false
+@export var boostAmount: float = 20
 
-var visualBar:float = 0
+@export var infiniteBoost: bool = false
 
-var initial_unit_scale:Vector2
+var linked_player_id: RID 
 
-var texture_cached:TextureRect = TextureRect.new()
+var growMode = false
 
-var linked_player_id:RID 
+var visualBar = 0
+
+var maxBoost = 0
 
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	texture_cached.stretch_mode = TextureRect.STRETCH_KEEP_ASPECT_CENTERED
-	texture_cached.expand_mode = TextureRect.EXPAND_FIT_HEIGHT_PROPORTIONAL
-	texture_cached.texture = boost_bar_red
-	
-	#Connect to the singleton to look for a player to link to
-	FlowStatSingleton.connect("boost_updated", setupBoostBar)
-
-func setupBoostBar(id:RID) -> void:
-	#make sure this is the right player
-	if id != linked_player_id:
-		return
-	#setup the boost bar to reflect the boost amount of the character
-	var boostAmount:float = FlowStatSingleton.getBoostAmount(id)
-	for i in range(boostAmount):
-		add_child(texture_cached.duplicate())
-	initial_unit_scale = get_child(0).scale
-	
-	#Detach from setup now that we're set up, and connect to update signals
-	FlowStatSingleton.disconnect("boost_updated", setupBoostBar)
+func _ready():
 	FlowStatSingleton.connect("boost_updated", updateBoostBar)
+	for i in range(20):
+		barItems.append(barUnit.instantiate())
+		barItems[i].position = Vector2(0,-14*i-16)
+		add_child(barItems[i])
+
+func _process(_delta):
+	if visualBar < boostAmount and visualBar <= 60:
+		visualBar += 0.5
+		barItems[floor(fmod(visualBar-1.0,20))].scale.x = 4
+	else:
+		visualBar = boostAmount
+	
+	boostAmount = clampf(boostAmount,0,60)
+	
+	if boostAmount < 60 and infiniteBoost:
+		boostAmount = 60
+	
+	var index = 0
+	for i in barItems:
+		index += 1
+		var colorVal = floor(visualBar/20)+(1 if fmod(visualBar,20) > index*1.0 else 0)
+		var mat: ShaderMaterial = i.material
+		mat.set_shader_parameter("uv_offset",Vector2(0,3-colorVal))
+		i.scale.x = lerp(i.scale.x,2.0,0.2)
 
 func updateBoostBar(id:RID) -> void:
 	if id != linked_player_id:
 		return
 	
-	var boostAmount:float = FlowStatSingleton.getBoostAmount(id)
+	maxBoost = FlowStatSingleton.getBoostMax(id)
 	
-	if visualBar < boostAmount and visualBar <= 60:
-		visualBar += 0.5
-		get_child(floorf(fmod(visualBar - 2, get_child_count()))).scale.x = 4
-	else:
-		visualBar = boostAmount
+	boostAmount = FlowStatSingleton.getBoostAmount(id)
 	
-	if infiniteBoost:
-		boostAmount = 60
-	
-	boostAmount = clampf(boostAmount, 0, 60)
-	
-	var index:int = 0
-	for i:TextureRect in get_children():
-		
-		var colorVal:float = floorf(visualBar / get_child_count()) + (1 if floorf(fmod(visualBar, get_child_count())) > index else 0)
-		
-		texture_cached.scale = i.scale
-		match colorVal:
-			3:
-				texture_cached.texture = boost_bar_white
-			2:
-				texture_cached.texture = boost_bar_blue
-			1:
-				texture_cached.texture = boost_bar_yellow
-			0:
-				texture_cached.texture = boost_bar_red
-		i = texture_cached.duplicate()
-		
-		#i.get_node("TextureRect").position.y = -24 + 8 * colorVal
-		var anim:Tween = create_tween()
-		anim.tween_property(get_child(index), "scale", Vector2(2.0, initial_unit_scale.y), 0.2)
-		index += 1
-		#i.scale.x = lerpf(i.scale.x, 2.0, 0.2)
-
-#func changeBy(x:float) -> void:
-#	boostAmount += x
-#	updateBoostBar()
+	boostAmount = (boostAmount*1.0/maxBoost)*60.0
